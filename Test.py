2fd14379acffb1131ac62624d8db4e9bc762d1fb
DivyashaAgrawal <divyashaselina7@gmail.com>
2019-01-31 15:17:22 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -18,14 +18,18 @@ import pytesseract
 import os
 from pdf2image import convert_from_path
 import pprint
+import collections
 try:
 	from StringIO import StringIO
 except ImportError:
 	from io import StringIO
 
 class MyParser(object):
+
 	def __init__(self, pdf):
-		parser = PDFParser(open(pdf, 'rb'))###read the pdf and read the text
+		
+		###read the pdf and read the text
+		parser = PDFParser(open(pdf, 'rb'))
 		document = PDFDocument(parser)
 		if not document.is_extractable:
 			raise PDFTextExtractionNotAllowed
@@ -39,91 +43,146 @@ class MyParser(object):
 			interpreter.process_page(page)
 		self.records = []
 		lines = retstr.getvalue().splitlines()
-		f= open('OCR_miner.txt','w')###save the text in a text file
+	
+		
+		words=[]
 		for i in lines:
 			i=i.replace(" ","")
-			print(i)
-			f.write(i)
-			f.write(" ")
-		f.close()
-		pages = convert_from_path(pdf, 500)###Convert pdf to png
+			
+		for i in lines:
+			words.append(i)
+		words.pop(0)
+		#print(words)
+		method=[]
+		matching = [s for s in words if "(" in s]
+		#print(matching)
+		#print(len(matching))
+		#print("\n\n\n")
+			
+		###Convert pdf to png
+		 
+		pages = convert_from_path(pdf, 500)
 		for page in pages:
-			page.save('input.jpg', 'JPEG')
-		image = cv2.imread('input.jpg')###read the image
-		img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		
+			page.save('input.png', 'PNG')
+
+		###read the image
+
+		image = cv2.imread('input.png')
+		img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)#Convert to grayscale
 		
 		###read the templates
-		right = cv2.imread('Images/arrow_right.jpg',0)
+		right = cv2.imread('Images/arrow_right.png',0)
 		wr, hr = right.shape[::-1]
-		left=cv2.imread('Images/arrow_left.jpg',0)
+		left=cv2.imread('Images/arrow_left.png',0)
 		wl, hl = left.shape[::-1]
 		slf=cv2.imread('Images/self_arrow.jpg',0)
 		ws, hs = slf.shape[::-1]
 		
+
+		d={'key':'value'}
+
 		###Template Matching
 		res = cv2.matchTemplate(img_gray,right,cv2.TM_CCOEFF_NORMED)
 		res1= cv2.matchTemplate(img_gray,left,cv2.TM_CCOEFF_NORMED)
 		res2= cv2.matchTemplate(img_gray,slf,cv2.TM_CCOEFF_NORMED)
-		threshold = 0.85
 		
-		loc = np.where(res >= threshold) 
+		###To get multiple instances set a threshold
+		threshold = 0.85
+		loc = np.where(res >= threshold)
+		pp = pprint.PrettyPrinter(indent=4)
 		loc1=np.where(res1 >= threshold)
 		loc2 = np.where( res2 >= threshold)
 		
 		arr=[[]]
-		
 		###Draw rectangles around each instance in the image
 		for pt in zip(*loc[::-1]):
 			cv2.rectangle(image, pt, (pt[0] + wr, pt[1] + hr), (0,0,255), 1)
 			arr.append([pt[0],pt[1],pt[0] + wr,pt[1] + hr])
-		
-		arr=arr.pop(0)
+
+		arr.pop(0)
 		arr=np.array(arr)
-		print(arr)
-		
+	
 		images = [("input.png",arr)]
 		iter_num= 1
-		images = images*iter_num
-		t1 = time.time()
+		images = images*iter_num  
     
 		###Loop over the images
-		for (i, (imagePath, boundingBoxes)) in enumerate(images):
-    			###Load the image 
+		for (i, (imagePath, Boxes)) in enumerate(images):
+    			
+			###Load the image
 			image = cv2.imread(imagePath)
 			orig = image.copy()
 	 
 			###Loop over the bounding boxes for each image and draw them
-			for (startX, startY, endX, endY) in boundingBoxes:
+			for (startX, startY, endX, endY) in Boxes:
 				cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 0, 255), 2)
 	    
 			###Non-maximum suppression on the bounding boxes
-			pick = non_max_suppression_fast(boundingBoxes, probs=None, overlapThresh=0.3)
+			pick = non_max_suppression_fast(Boxes, probs=None, overlapThresh=0.3)
 
-			print ("[x] after applying non-maximum, %d bounding boxes" % (len(pick)))   
+			#print ("[%d bounding boxes" % (len(pick)))   
 	    
-			###Loop over the picked bounding boxes and draw them
+			###Loop over the picked bounding box and append them to array.
 			for (startX, startY, endX, endY) in pick:
-				cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
+				d[startY]='right'
+
+
+		arr1=[[]]
 	    
 		for pt in zip(*loc1[::-1]):
 			cv2.rectangle(image, pt, (pt[0] + wl, pt[1] + hl), (0,255,0), 1)
-			
+			arr1.append([pt[0],pt[1],pt[0] + wr,pt[1] + hr])
+
+		arr1.pop(0)
+		arr1=np.array(arr1)
+	
+		images = [("input.png",arr1)]
+		iter_num= 1
+		images = images*iter_num  
+    
+		###Loop over the images
+		for (i, (imagePath, Boxes)) in enumerate(images):
+    			
+			###Load the image
+			image = cv2.imread(imagePath)
+			orig = image.copy()
+	 
+			###Loop over the bounding boxes for each image and draw them
+			for (startX, startY, endX, endY) in Boxes:
+				cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
+	    
+			###Non-maximum suppression on the bounding boxes
+			pick1 = non_max_suppression_fast(Boxes, probs=None, overlapThresh=0.3)
+
+			#print ("%d bounding boxes" % (len(pick1)))   
+	    
+			###Loop over the picked bounding boxes and draw them
+			for (startX, startY, endX, endY) in pick1:
+				d[startY]='left'
+		del d['key']
+		od = collections.OrderedDict(sorted(d.items()))
+		dir=[]
+		for k, v in od.items(): 
+			dir.append(v)
+		#dir.pop(0)
+		#print(dir)
+		print("\n\n\n")
+		final=zip(matching,dir)
+		print(final)
+		#print(len(final))
+		"""###save the text in a text file
+		f= open('EA_miner.txt','w')
+		for i in (matching,dir):
+			f.write(matching[i], "----", dir[i])
+			f.write("\n")
+		f.close()"""
+
 		for pt in zip(*loc2[::-1]):
 			cv2.rectangle(image, pt, (pt[0] + ws, pt[1] + hs), (255,0,0), 1)
-			
-		cv2.imwrite('arrow_extracted.jpg',image)
-		
+
+		 		
 	def handle_line(self, line):
 		self.records.append(line)		 
 if __name__ == '__main__':
 	p = MyParser(sys.argv[1])
 	print ('\n'.join(p.records))
-
-
-
-
-
-
-
-
