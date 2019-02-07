072b051972b91ffb52cf200d8134cfb7559a4dca
DivyashaAgrawal <divyashaselina7@gmail.com>
2019-02-07 15:20:52 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from nms import non_max_suppression_slow, non_max_suppression_fast
+
 from pdfminer.pdfpage import PDFTextExtractionNotAllowed
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfinterp import PDFPageInterpreter
@@ -12,26 +12,22 @@ from matplotlib import pyplot as plt
 from pdfminer.pdfpage import PDFPage
 from pdfminer.layout import LAParams
 from collections import defaultdict
+from extraction import extract
 from PIL import Image
-import pytesseract
+from mongo import *
 import numpy as np
 import collections
 import argparse
 import pprint
 import time
-import cv2
 import sys
+import cv2
 import os
 
 try:
 	from StringIO import StringIO
 except ImportError:
 	from io import StringIO
-
-
-
-
-
 
 class MyParser:
 
@@ -88,149 +84,31 @@ class MyParser:
 		pages = convert_from_path(pdf, 500)
 		for page in pages:
 			page.save('input.png', 'PNG')
-
-		
-		#Read the image and convert to grayscale
 		image = cv2.imread('input.png')
-		img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		
-		#Read the templates
-		right = cv2.imread('Images/arrow_right.png',0)#right arrow
-		wr, hr = right.shape[::-1]
-		left=cv2.imread('Images/arrow_left.png',0)#left arrow
-		wl, hl = left.shape[::-1]
-		slf=cv2.imread('Images/self_arrow.jpg',0)#self arrow
-		ws, hs = slf.shape[::-1]
-		template = cv2.imread('Images/smallbox.png',0)#boxes
-		w, h = template.shape[::-1]
-		template2 = cv2.imread('Images/box.png',0)#components
-		w2, h2 = template2.shape[::-1]
-
-		
-		#Template Matching
-		res = cv2.matchTemplate(img_gray,right,cv2.TM_CCOEFF_NORMED)
-		res1= cv2.matchTemplate(img_gray,left,cv2.TM_CCOEFF_NORMED)
-		res2= cv2.matchTemplate(img_gray,slf,cv2.TM_CCOEFF_NORMED)
-		res3 = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-		res4 = cv2.matchTemplate(img_gray,template2,cv2.TM_CCOEFF_NORMED)
-
-		#To get multiple instances set a threshold
-		threshold = 0.85
-		loc = np.where(res >= threshold)
-		loc1=np.where(res1 >= threshold)
-		loc2 = np.where( res2 >= threshold)
-		loc3 = np.where( res3 >= threshold)
-		loc4 = np.where( res4 >= 0.9)
 		
 		orient={'key':'value'}#dictionary for orientation of the arrows
 
-		###Create bounding boxes over every right arrow
-		arr=[[]]
-		#Draw rectangles around each instance in the image
-		for pt in zip(*loc[::-1]):
-			cv2.rectangle(image, pt, (pt[0] + wr, pt[1] + hr), (0,0,255), 1)
-			arr.append([pt[0],pt[1],pt[0] + wr,pt[1] + hr])
+		#read the right arrow
+		right = cv2.imread('Images/arrow_right.png',0)#right arrow
+		pick = extract(image,right,0.85)
+		
+		#read the left arrow
+		left = cv2.imread('Images/arrow_left.png',0)#left arrow
+		pick1 = extract(image,left,0.85)
 
-		arr.pop(0)
-		arr=np.array(arr)
-		images = [("input.png",arr)]
-		iter_num= 1
-		images = images*iter_num  
-    
-		#Loop over the images
-		for (imagePath, Boxes) in images:
-    			
-			#Load the image
-			image = cv2.imread(imagePath)
-			orig = image.copy()
-	 
-			#Loop over the bounding boxes for each image and draw them
-			for (startX, startY, endX, endY) in Boxes:
-				cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 0, 255), 2)
-	    
-			#Non-maximum suppression on the bounding boxes
-			pick = non_max_suppression_fast(Boxes, probs=None, overlapThresh=0.3)
-
-	    
-			#Loop over the picked bounding box and append them to array.
-			for (startX, startY, endX, endY) in pick:#for right
+		#read the boxes
+		template = cv2.imread('Images/smallbox.png',0)#boxes
+		pick3 = extract(image,template,0.85)
+		
+		#read the components
+		template2 = cv2.imread('Images/box.png',0)#components
+		pick2 = extract(image,template2,0.9)
+		
+		for (startX, startY, endX, endY) in pick:#for right
 				orient[startY]='left to right'
 		
-		###Create bounding boxes over every left arrow
-		arr1=[[]]
-		for pt in zip(*loc1[::-1]):
-			cv2.rectangle(image, pt, (pt[0] + wl, pt[1] + hl), (0,255,0), 1)
-			arr1.append([pt[0],pt[1],pt[0] + wr,pt[1] + hr])
-
-		arr1.pop(0)
-		arr1=np.array(arr1)
-	
-		images = [("input.png",arr1)]
-		iter_num= 1
-		images = images*iter_num  
-    
-		#Loop over the images
-		for (imagePath, Boxes) in images:
-    			
-			#Load the image
-			image = cv2.imread(imagePath)
-			orig = image.copy()
-	 
-			#Loop over the bounding boxes for each image and draw them
-			for (startX, startY, endX, endY) in Boxes:
-				cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
-	    
-			#Non-maximum suppression on the bounding boxes
-			pick1 = non_max_suppression_fast(Boxes, probs=None, overlapThresh=0.3)#for left
- 			
-			#Loop over the picked bounding box and append them to array
-			for (startX, startY, endX, endY) in pick1:
+		for (startX, startY, endX, endY) in pick1:
 				orient[startY]='right to left'
-		
-
-		###Create bounding boxes over every component
-		arr2=[]
-		for pt in zip(*loc4[::-1]):
-			cv2.rectangle(image, pt, (pt[0] + w2, pt[1] + h2), (0,0,0), 2)
-			arr2.append([pt[0],pt[1],pt[0] + w2,pt[1] + h2])
-		arr2.pop(0)
-		arr2=np.array(arr2)
-		images = [("input.png",arr2)]
-		iter_num= 1
-		images = images*iter_num  
-    
-		#Loop over the images
-		for(imagePath, Boxes) in images:
-			image = cv2.imread(imagePath)
-			orig = image.copy()
-	 
-			#Loop over the bounding boxes for each image and draw them
-			for (startX, startY, endX, endY) in Boxes:
-				cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
-	    
-			pick2 = non_max_suppression_fast(Boxes, probs=None, overlapThresh=0.3)#for components
-
-		###Create bounding boxes over every smallboxes
-		arr3=[]
-		for pt in zip(*loc3[::-1]):
-			cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-			arr3.append([pt[0],pt[1],pt[0] + w,pt[1] + h])
-		arr3.pop(0)
-		arr3=np.array(arr3)
-		images = [("input.png",arr3)]
-		iter_num= 1
-		images = images*iter_num  
-    
-		#Loop over the image
-		for (imagePath, Boxes) in images:
-			image = cv2.imread(imagePath)
-			orig = image.copy()
-	 
-			#Loop over the bounding boxes for each image and draw them
-			for (startX, startY, endX, endY) in Boxes:
-				cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
-	    
-			pick3 = non_max_suppression_fast(Boxes, probs=None, overlapThresh=0.3)#for smallboxes
 		
 		#Reversing the lists to get correct index
 		pick3[:] = pick3[::-1]
@@ -293,6 +171,7 @@ class MyParser:
 		d = {}
 		for k in orient:
     			d[k] = tuple(d[k] for d in ds)
+		
 		#correction of the orientation
 		l=list()
 		final=[]
@@ -351,14 +230,13 @@ class MyParser:
 		f.write('\n\n')
 		
 		f.close()
-		
-		"""#for self arrow
-		for pt in zip(*loc2[::-1]):
-			cv2.rectangle(image, pt, (pt[0] + ws, pt[1] + hs), (255,0,0), 1)"""
 
-		 		
+
 	def handle_line(self, line):
-		self.records.append(line)		 
+		self.records.append(line)
+		
+
+		 
 if __name__ == '__main__':
 	p = MyParser(sys.argv[1])
 	print ('\n'.join(p.records))
