182fb465ba54d33e78327f98d6e0ea667ee27206
DivyashaAgrawal <divyashaselina7@gmail.com>
2019-02-28 16:25:16 +0530

    Update contouring.py

--- contouring.py
+++ contouring.py
@@ -1,12 +1,29 @@
 import cv2
-import time
+import imutils
 import numpy as np
 from pprint import pprint
+
+
+
+def sort_contours(cnts):
+	# initialize the reverse flag and sort index
+	reverse = False
+	i = 1
+
+	# construct the list of bounding boxes and sort them from top to
+	# bottom
+	boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+	(cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+		key=lambda b:b[1][i], reverse=reverse))
+ 
+	# return the list of sorted contours and bounding boxes
+	return (cnts, boundingBoxes)
 
 
 #width=600.0
 #height=420.0
 #margin=0.0
+
 corners = np.array([[[0.0, 0.0]],[[0.0,420.0 + 0.0]],[[ 600.0 + 0.0, 420.0 + 0.0]],[[ 600.0 + 0.0, 0.0]]])
 pts_dst = np.array( corners, np.float32 )
 rgb=cv2.imread('input.png',1)
@@ -15,9 +32,11 @@ gray = cv2.bilateralFilter( gray, 1, 10, 120 )
 edges  = cv2.Canny( gray, 10, 250 )
 kernel = cv2.getStructuringElement( cv2.MORPH_RECT, ( 7, 7 ) )
 closed = cv2.morphologyEx( edges, cv2.MORPH_CLOSE, kernel )
-_ , contours, _ = cv2.findContours(closed,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE )
+_ , cnts, _ = cv2.findContours(closed,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE )
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
 app=[]
-for cont in contours:
+(cnts, boundingBoxes) = sort_contours(cnts)
+for cont in cnts:
 	if (cv2.contourArea( cont )>5000):
 		arc = cv2.arcLength( cont,True)
 		approx = cv2.approxPolyDP( cont, 0.1 * arc, True)
@@ -28,5 +47,9 @@ for cont in contours:
 			h, status = cv2.findHomography( pts_src, pts_dst )
 			out = cv2.warpPerspective( rgb, h, ( int( 600.0 + 0.0 * 2 ), int( 420.0 + 0.0 * 2 ) ) )
 			cv2.drawContours( rgb, [approx], -1, ( 255, 0, 0 ), 2 )
+
+
+
 pprint(app)
 cv2.imwrite('Image.png',rgb)
+
