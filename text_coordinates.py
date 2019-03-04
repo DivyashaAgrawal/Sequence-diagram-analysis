174dc6fc80c997622902b4e2586ec4b6386751a7
DivyashaAgrawal <divyashaselina7@gmail.com>
2019-03-04 18:07:34 +0530

    Update text_coordinates.py

--- text_coordinates.py
+++ text_coordinates.py
@@ -1,16 +1,15 @@
-from .pdfdocument import PDFNoOutlines
-from .pdfdocument import PDFDocument
-from .pdfparser import PDFParser
-from .pdfinterp import PDFPageInterpreter
-from .pdfinterp import PDFResourceManager
-from .layout import LAParams
-from .layout import LTAnno
+from pdfminer.pdfdocument import PDFDocument, PDFNoOutlines
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LTPage, LTChar, LTAnno, LAParams, LTTextBox, LTTextLine
 
-##Inherited function from PDFAggregator
+##Inherited function
 class PDFPageDetailed(PDFPageAggregator):
     def __init__(self, rsrcmgr, pageno=1, laparams=None):
         PDFPageAggregator.__init__(self, rsrcmgr, pageno=pageno, laparams=laparams)
         self.rows = []
+        self.comments = []
         self.page_number = 0
     def receive_layout(self, ltpage):        
         def render(item, page_number):
@@ -18,18 +17,21 @@ class PDFPageDetailed(PDFPageAggregator):
                 for child in item:
                     render(child, page_number)
             elif isinstance(item, LTTextLine):
-                child_str = ''
+                child_str = ''a
                 for child in item:
                     if isinstance(child, (LTChar, LTAnno)):
                         child_str += child.get_text()
+                    if isinstance(child,LTChar):
+                        font = child.fontname
+                        fnt=(font.split("+"))[1].split("U")[0]
                 child_str = ' '.join(child_str.split()).strip()
                 if child_str:
-                    row = ( item.bbox[0], item.bbox[1], item.bbox[2], item.bbox[3], child_str) # bbox == (x1, y1, x2, y2)
+                    row = ( item.bbox[0], item.bbox[1], item.bbox[2], item.bbox[3], child_str , fnt ) # bbox == (x1, y1, x2, y2)
                     self.rows.append(row)
                 for child in item:
                     render(child, page_number)
             return
         render(ltpage, self.page_number)
         self.page_number += 1
-        self.rows = sorted(self.rows, key = lambda x: (-x[1]))
+        self.rows = sorted(self.rows, key = lambda x: ( -x[1], x[0] , x[5]))
         self.result = ltpage
