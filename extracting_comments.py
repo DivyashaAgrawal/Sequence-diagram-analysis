d6b5960538d0df7d1e8534a859cf9fc7859bfeb7
DivyashaAgrawal <divyashaselina7@gmail.com>
2019-03-06 23:32:15 +0530

    commiting changes

--- extracting_comments.py
+++ extracting_comments.py
@@ -31,49 +31,13 @@ codec = 'utf-8'
 # and merging the ones close to each other.
 device = TextConverter(rsrcmgr, retstr,codec = codec,laparams = laparams) 
 interpreter = PDFPageInterpreter(rsrcmgr, device)
-for page in PDFPage.create_pages(document):
-	interpreter.process_pagit ge(page)
+
+for page in PDFPage.create_pages(document):	
+	interpreter.process_page(page)
+
 records = [] # The text list
 lines = retstr.getvalue().splitlines()
 words=[]
 for i in lines:
 			words.append(i)
 pprint(words)
-"""values = set(map(lambda x:x[0], words))
-forcomments = [[y[4] for y in words if y[0]==x] for x in values] # will have same x1 that is words[0]
-
-forcomponents=[]
-f=0
-for y in range(len(words)):
-	if ((y+1)!=len(words) and words[y][1]==words[y+1][1] and words[y][3]==words[y+1][3]):
-		p=words[y+1][4]
-		forcomponents.append(words[y][4])
-		forcomponents.append(p)
-			
-forcomponents = list(collections.OrderedDict.fromkeys(forcomponents))		
-
-		forcomments=[]
-
-		for i in range(len(comments)):
-			f=0
-			for j in range(len(components)):
-				if(comments[i]!=components[j]):
-					f=1
-				else:
-					f=0
-					break
-			if(f==1):
-				forcomments.append(comments[i])
-
-		forforcomments=[]
-		for i in forcomments:
-			f=0
-			for j in functions:
-				if(i!=j):
-					f=1
-				else:
-					f=0
-					break
-			if(f==1):
-				forforcomments.append(i)"""
-	
