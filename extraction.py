4f0b647247f6b15f47ea0525acf44a8a5b7cbd2b
DivyashaAgrawal <divyashaselina7@gmail.com>
2019-03-06 19:30:41 +0530

    Update extraction.py

--- extraction.py
+++ extraction.py
@@ -5,12 +5,11 @@ import numpy as np
 from PIL import Image
 from nms import non_max_suppression_slow, non_max_suppression_fast
 
-def extract(image,string):
-	
-	image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def extract(image,string,thresh):
+	copy=image.copy()
+	image_gray = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
 	#Read the templates
 	template_data=[]
-	
 	#make a list of all template images from a directory
 	files= glob.glob(string + '/*')
 	for myfile in files:
@@ -21,13 +20,14 @@ def extract(image,string):
 	for tmp in template_data:
 		(tW, tH) = tmp.shape[::-1]
 		res = cv2.matchTemplate(image_gray, tmp, cv2.TM_CCOEFF_NORMED)
-		loc = np.where(res >= 0.9)
+		loc = np.where(res >=thresh)
 
-	
 		#Draw rectangles around each instance in the image
 		for pt in zip(*loc[::-1]):
-			cv2.rectangle(image, pt, (pt[0] + tW, pt[1] + tW), (0,0,255), 1)
+			#cv2.rectangle(copy, pt, (pt[0] + tW, pt[1] + tW), (0,0,255), 1)
 			arr.append([pt[0],pt[1],pt[0] + tW,pt[1] + tH])
+	
+	
 	arr.pop(0)
 	arr=np.array(arr)
 	images = [('input.png',arr)]
@@ -37,14 +37,14 @@ def extract(image,string):
 	#Loop over the images
 	for (imagePath, Boxes) in images:	    			
 		#Load the image
-		image = cv2.imread(imagePath)
-		orig = image.copy()
+		image1 = cv2.imread(imagePath)
+		orig = image1.copy() 
 		 
 		#Loop over the bounding boxes for each image and draw them
 		for (startX, startY, endX, endY) in Boxes:
-			cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 0, 255), 2)
-		    
+			cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 0, 255), 1)
 		#Non-maximum suppression on the bounding boxes
 		pick = non_max_suppression_fast(Boxes, probs=None, overlapThresh=0.3)
-	print(pick)	
+	
 	return pick
+
